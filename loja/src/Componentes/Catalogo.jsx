a1caf54e3ef7052b9c8a1675ed17fd38c4db58f7
willrizzo <willianrizzoratao10@gmail.com>
2023-04-17 16:21:42 -0300

    catalogo details

--- loja/src/Componentes/Catalogo.jsx
+++ loja/src/Componentes/Catalogo.jsx
@@ -2,23 +2,39 @@ import Produtos from "./Produtos";
 
 function Catalogo(){
     return(
-        <div className="grid grid-cols-4 gap-x-4 mx-16 my-4">
-            <Produtos img="https://phytoterapica.vtexassets.com/arquivos/ids/155798/oleo-essencial-lavanda-10ml-phytoterapica-01.jpg?v=1768154098" nome="Óleo Essencial de Lavanda (gt. França) - 10ml" preco="R$ 60,00" desc="(ou em até 3x R$ 20,00 sem juros)" />
-            
-            <Produtos img="https://phytoterapica.vtexassets.com/arquivos/ids/155798/oleo-essencial-lavanda-10ml-phytoterapica-01.jpg?v=1768154098" nome="Óleo Essencial de Lavanda (gt. França) - 10ml" preco="R$ 60,00" desc="(ou em até 3x R$ 20,00 sem juros)" />
-           
-            <Produtos img="https://phytoterapica.vtexassets.com/arquivos/ids/155798/oleo-essencial-lavanda-10ml-phytoterapica-01.jpg?v=1768154098" nome="Óleo Essencial de Lavanda (gt. França) - 10ml" preco="R$ 60,00" desc="(ou em até 3x R$ 20,00 sem juros)" />
-           
-            <Produtos img="https://phytoterapica.vtexassets.com/arquivos/ids/155798/oleo-essencial-lavanda-10ml-phytoterapica-01.jpg?v=1768154098" nome="Óleo Essencial de Lavanda (gt. França) - 10ml" preco="R$ 60,00" desc="(ou em até 3x R$ 20,00 sem juros)" />
+        <div className="grid grid-cols-4 gap-x-4 mx-14 my-4">
 
-            <Produtos img="https://phytoterapica.vtexassets.com/arquivos/ids/155798/oleo-essencial-lavanda-10ml-phytoterapica-01.jpg?v=1768154098" nome="Óleo Essencial de Lavanda (gt. França) - 10ml" preco="R$ 60,00" desc="(ou em até 3x R$ 20,00 sem juros)" />
+            <Produtos img="https://phytoterapica.vtexassets.com/arquivos/ids/155798/oleo-essencial-lavanda-10ml-phytoterapica-01.jpg?v=1768154098" nome="Óleo Essencial de Lavanda (gt. França) - 10ml" preco="R$ 60,00" desc="(ou em até 3x de R$ 20,00 sem juros)" />
             
-            <Produtos img="https://phytoterapica.vtexassets.com/arquivos/ids/155798/oleo-essencial-lavanda-10ml-phytoterapica-01.jpg?v=1768154098" nome="Óleo Essencial de Lavanda (gt. França) - 10ml" preco="R$ 60,00" desc="(ou em até 3x R$ 20,00 sem juros)" />
+            <Produtos img="https://phytoterapica.vtexassets.com/arquivos/ids/155814/oleo-essencial-geranio-5ml-phytoterapica-01.jpg?v=1768156042" nome="Óleo Essencial de Gerânio - 5ml" preco="R$ 44,00" desc="(ou em até 3x de R$ 14,66 sem juros)" />
            
-            <Produtos img="https://phytoterapica.vtexassets.com/arquivos/ids/155798/oleo-essencial-lavanda-10ml-phytoterapica-01.jpg?v=1768154098" nome="Óleo Essencial de Lavanda (gt. França) - 10ml" preco="R$ 60,00" desc="(ou em até 3x R$ 20,00 sem juros)" />
+            <Produtos img="https://phytoterapica.vtexassets.com/arquivos/ids/155804/oleo-essencial-alecrim-10ml-phytoterapica-01.jpg?v=1768156028" nome="Óleo Essencial de Alecrim (qt. Cineol) - 10ml" preco="R$ 37,00" desc="(ou em até 3x de R$ 12,33 sem juros)" />
            
-            <Produtos img="https://phytoterapica.vtexassets.com/arquivos/ids/155798/oleo-essencial-lavanda-10ml-phytoterapica-01.jpg?v=1768154098" nome="Óleo Essencial de Lavanda (gt. França) - 10ml" preco="R$ 60,00" desc="(ou em até 3x R$ 20,00 sem juros)" />
+            <Produtos img="https://phytoterapica.vtexassets.com/arquivos/ids/155806/oleo-essencial-hortela-pimenta-10ml-phytoterapica-01.jpg?v=1768156016" nome="Óleo Essencial de Hortelã-Pimenta (Menta) - 10ml" preco="R$ 52,00" desc="(ou em até 3x de R$ 17,33 sem juros)" />
 
+            <Produtos img="https://phytoterapica.vtexassets.com/arquivos/ids/155808/oleo-essencial-melaleuca-tea-tree-10ml-phytoterapica-01.jpg?v=1768153661" nome="Óleo Essencial de Melaleuca (Tea Tree) - 10ml" preco="R$ 50,00" desc="(ou em até 3x de R$ 16,66 sem juros)" />
+            
+            <Produtos img="https://phytoterapica.vtexassets.com/arquivos/ids/155886/oleo-essencial-laranja-organico-10ml-phytoterapica-01.jpg?v=1768153082" nome="Óleo Essencial de Laranja (Orgânico) - 10ml" preco="R$ 26,00" desc="(ou em até 3x de R$ 8,66 sem juros)" />
+           
+            <Produtos img="https://phytoterapica.vtexassets.com/arquivos/ids/155820/oleo-essencial-eucalipto-globulus-10ml-phytoterapica-01.jpg?v=1768154484" nome="Óleo Essencial de Eucalipto Globulus - 10ml" preco="R$ 35,20" desc="(ou em até 3x de R$ 11,73 sem juros)" />
+           
+            <Produtos img="https://phytoterapica.vtexassets.com/arquivos/ids/155838/oleo-essencial-bergamota-5ml-phytoterapica-01.jpg?v=1768151722" nome="Óleo Essencial de Bergamota - 5ml" preco="R$ 40,00" desc="(ou em até 3x de R$ 13,33 sem juros)" />
+
+            <Produtos img="https://phytoterapica.vtexassets.com/arquivos/ids/156013/Mockup_Caixa_OE_Melaleuca_5ml_1000x1000px.jpg?v=1768147544" nome="Óleo Essencial de Melaleuca (Tea Tree) - 5ml" preco="R$ 39,20" desc="(ou em até 3x de R$ 13,06 sem juros)" />
+            
+            <Produtos img="https://phytoterapica.vtexassets.com/arquivos/ids/156000/Mockup_Caixa_OE_Lavanda_5ml_1000x1000px.jpg?v=1768154482" nome="Óleo Essencial de Lavanda (gt. França) - 5ml" preco="R$ 47,02" desc="(ou em até 3x de R$ 15,67 sem juros)" />
+           
+            <Produtos img="https://phytoterapica.vtexassets.com/arquivos/ids/155415/_leo_essencial_de_lim_o-tahiti_10ml_-_phytoterapica_1_.jpg?v=1768157424" nome="Óleo Essencial de Limão-Tahiti - 10ml" preco="R$ 34,00" desc="(ou em até 3x R$ 11,40 sem juros)" />
+           
+            <Produtos img="https://phytoterapica.vtexassets.com/arquivos/ids/155552/oleo-essencial-lemongrass-organico-10ml-phytoterapica-caixa.jpg?v=1768157059" nome="Óleo Essencial de Lemongrass (Orgânico) - 10ml" preco="R$ 39,00" desc="(ou em até 3x de R$ 13,00 sem juros)" />
+
+            <Produtos img="https://phytoterapica.vtexassets.com/arquivos/ids/155798/oleo-essencial-lavanda-10ml-phytoterapica-01.jpg?v=1768154098" nome="Óleo Essencial de Lavanda (gt. França) - 10ml" preco="R$ 60,00" desc="(ou em até 3x de R$ 20,00 sem juros)" />
+            
+            <Produtos img="https://phytoterapica.vtexassets.com/arquivos/ids/155814/oleo-essencial-geranio-5ml-phytoterapica-01.jpg?v=1768156042" nome="Óleo Essencial de Gerânio - 5ml" preco="R$ 44,00" desc="(ou em até 3x de R$ 14,66 sem juros)" />
+           
+            <Produtos img="https://phytoterapica.vtexassets.com/arquivos/ids/155804/oleo-essencial-alecrim-10ml-phytoterapica-01.jpg?v=1768156028" nome="Óleo Essencial de Alecrim (qt. Cineol) - 10ml" preco="R$ 37,00" desc="(ou em até 3x de R$ 12,33 sem juros)" />
+           
+           <Produtos img="https://phytoterapica.vtexassets.com/arquivos/ids/155806/oleo-essencial-hortela-pimenta-10ml-phytoterapica-01.jpg?v=1768156016" nome="Óleo Essencial de Hortelã-Pimenta (Menta) - 10ml" preco="R$ 52,00" desc="(ou em até 3x de R$ 17,33 sem juros)" />
 
 
            

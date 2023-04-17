aa0fd152e658e1ae5e3e36e7d69c326aa920f832
willrizzo <willianrizzoratao10@gmail.com>
2023-04-17 14:57:24 -0300

    nav pronta

--- loja/src/App.jsx
+++ loja/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './Componentes/Header'
+import Nav from './Componentes/Nav'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +9,7 @@ function App() {
   return (
     <div>
       <Header />
+      <Nav/>
     </div>
   )
 }
---
+++ loja/src/Componentes/Nav.jsx
@@ -0,0 +1,18 @@
+function Nav(){
+    return(
+        <div className="flex items-center justify-evenly w-full h-20 border-y font-thin">
+            <p>ÓLEOS ESSENCIAIS</p>
+            <p>ÓLEOS VEGETAIS</p>
+            <p>SINERGIAS</p>
+            <p>COSMÉTICOS NATURAIS</p>
+            <p>HIGIENE PESSOAL</p>
+            <p>AROMATIZADORES</p>
+            <p>SUPLEMENTOS</p>
+            <p>ACESSÓRIOS</p>
+            <p>OFERTAS</p>
+            <p>CONSULTOR(A)</p>
+            
+        </div>
+    )
+}
+export default Nav

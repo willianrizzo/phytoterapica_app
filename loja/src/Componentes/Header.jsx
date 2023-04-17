feaa8d3fe459bdec770851fadd7a397dc45a39cd
willrizzo <willianrizzoratao10@gmail.com>
2023-04-17 15:26:38 -0300

    modifications header

--- loja/src/Componentes/Header.jsx
+++ loja/src/Componentes/Header.jsx
@@ -4,12 +4,12 @@ import Cart from "../assets/cart.svg"
 
 function Header(){
     return(
-        <div className="flex w-full justify-between p-10 items-center border-y">
+        <div className="flex w-full justify-between p-14 items-center border-y">
             <div>
                 <img src={Logo} alt="" className="w-72 " />
             </div>
             
-            <div className="flex gap-8">
+            <div className="flex gap-12">
                 <input placeholder="Pesquisar" className="bg-stone-100 h-8 px-8 rounded-full" type="search" name="" id="" />
                 <input placeholder="-5% na primeira compra." className="bg-stone-100 h-8 px-8 rounded-full" type="" name="" id="" />
                 <img className="w-6" src={User} alt="" />

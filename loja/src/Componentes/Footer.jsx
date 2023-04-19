ed0c5eb9b9aa1af9ff68d11ee35f7f5e734065e1
willrizzo <willianrizzoratao10@gmail.com>
2023-04-19 11:37:55 -0300

    footer criando

--- loja/src/Componentes/Footer.jsx
+++ loja/src/Componentes/Footer.jsx
@@ -1,7 +1,10 @@
+import Logo from "../assets/logo.png"
 function Footer(){
     return(
-        <div>
-            
+        <div className="w-full h-52">
+            <div>
+                <img className="w-72 m-auto" src={Logo} alt="" />
+            </div>
         </div>
     )
 }

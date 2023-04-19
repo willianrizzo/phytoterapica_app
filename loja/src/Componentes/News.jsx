8c37fd47a184bb8e25bf22250dad10e2063b5d04
willrizzo <willianrizzoratao10@gmail.com>
2023-04-19 10:56:45 -0300

    Newslatter concluido

--- loja/src/Componentes/News.jsx
+++ loja/src/Componentes/News.jsx
@@ -1,14 +1,17 @@
 function News(){
     return(
-        <div className=" relative m-auto flex items-center w-full h-24 bg-gray-200 ">
-           <div>
-                <h3>INSCREVA-SE</h3>
-                <p>E RECEBA NOVIDADES E PROMOÇÕES</p>
+        <div className=" relative m-auto flex items-center w-full h-24 bg-gray-100 ">
+           <div className="m-auto flex justify-between gap-10">
+                <div className=" m-auto">
+                        <h3 className="text-lg">INSCREVA-SE</h3>
+                        <p className="font-thin">E RECEBA NOVIDADES E PROMOÇÕES</p>
+                </div>
+                <input className="p-2 rounded-full w-96" placeholder="Digite seu E-mail:" type="email" />
+                <button className="bg-[#a8c321] w-72 p-2 rounded-full text-[#fff] ">
+                        Assinar Newsletter
+                </button>
            </div>
-           <input placeholder="Digite seu E-mail:" type="email" />
-           <button>
-                 Assinar Newsletter
-            </button>
+           
         </div>
     )
 }
